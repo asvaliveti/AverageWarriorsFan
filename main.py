dd5cb30e2ccc629ce93743ee8025cdb433d6bf02
Anirudh Valiveti <asvaliveti@gmail.com>
2023-01-22 14:46:16 -0600

    created a simple chatbot

--- main.py
+++ main.py
@@ -1,115 +1,146 @@
-import nltk
-from nltk.stem.lancaster import LancasterStemmer
-import numpy
-import tflearn
-from tensorflow.python.framework import ops
-import random
 import json
-import pickle
-nltk.download('punkt')
+import string
+import random 
+import nltk
+import numpy as np
+from nltk.stem import WordNetLemmatizer 
+import tensorflow as tf 
+from tensorflow.keras import Sequential 
+from tensorflow.keras.layers import Dense, Dropout
+nltk.download("punkt")
+nltk.download("wordnet")
 
-stemmer = LancasterStemmer()
+data = {"intents": [
+    {"tag": "greeting",
+     "patterns": ["Hello", "How are you?", "Hi there", "Hi", "Whats up"],
+     "responses": ["Howdy Partner!", "Hello", "How are you doing?", "Greetings!", "How do you do?"],
+    },
+    {"tag": "age",
+     "patterns": ["how old are you?", "when is your birthday?", "when was you born?"],
+     "responses": ["I am 18 years old", "I was born in 2004", "My birthday is March 25th and I was born in 2004", "03/25/2004"]
+    },
+    {"tag": "date",
+     "patterns": ["what are you doing this weekend?",
+"do you want to hang out some time?", "what are your plans for this week"],
+     "responses": ["I am available all week", "I don't have any plans", "I am not busy"]
+    },
+    {"tag": "name",
+     "patterns": ["what's your name?", "what are you called?", "who are you?"],
+     "responses": ["My name is Ani", "I'm Ani", "Ani"]
+    },
+    {"tag": "goodbye",
+     "patterns": [ "bye", "g2g", "see ya", "adios", "cya"],
+     "responses": ["It was nice speaking to you", "See you later", "Speak soon!"]
+    }
+]}
 
-with open("intents.json") as file:
-    data = json.load(file)
+# initializing lemmatizer to get stem of words
+lemmatizer = WordNetLemmatizer()
+# Each list to create
+words = []
+classes = []
+doc_X = []
+doc_Y = []
+# Loop through all the intents
+# tokenize each pattern and append tokens to words, the patterns and
+# the associated tag to their associated list
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        tokens = nltk.word_tokenize(pattern)
+        words.extend(tokens)
+        doc_X.append(pattern)
+        doc_Y.append(intent["tag"])
+    
+    # add the tag to the classes if it's not there already 
+    if intent["tag"] not in classes:
+        classes.append(intent["tag"])
+# lemmatize all the words in the vocab and convert them to lowercase
+# if the words don't appear in punctuation
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in string.punctuation]
+# sorting the vocab and classes in alphabetical order and taking the # set to ensure no duplicates occur
+words = sorted(set(words))
+classes = sorted(set(classes))
 
-try:
-    with open("data.pickle", "rb") as f:
-        words, labels, training, output = pickle.load(f)
-except:
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
+# list for training data
+training = []
+out_empty = [0] * len(classes)
+# creating the bag of words model
+for idx, doc in enumerate(doc_X):
+    bow = []
+    text = lemmatizer.lemmatize(doc.lower())
+    for word in words:
+        bow.append(1) if word in text else bow.append(0)
+    # mark the index of class that the current pattern is associated
+    # to
+    output_row = list(out_empty)
+    output_row[classes.index(doc_Y[idx])] = 1
+    # add the one hot encoded BoW and associated classes to training 
+    training.append([bow, output_row])
+# shuffle the data and convert it to an array
+random.shuffle(training)
+training = np.array(training, dtype=object)
+# split the features and target labels
+train_X = np.array(list(training[:, 0]))
+train_y = np.array(list(training[:, 1]))
 
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent["tag"])
+# defining some parameters
+input_shape = (len(train_X[0]),)
+output_shape = len(train_y[0])
+epochs = 200
+# the deep learning model
+model = Sequential()
+model.add(Dense(128, input_shape=input_shape, activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation="relu"))
+model.add(Dropout(0.3))
+model.add(Dense(output_shape, activation = "softmax"))
+adam = tf.keras.optimizers.legacy.Adam(learning_rate=0.01, decay=1e-6)
+model.compile(loss='categorical_crossentropy',
+              optimizer=adam,
+              metrics=["accuracy"])
+print(model.summary())
+model.fit(x=train_X, y=train_y, epochs=200, verbose=1)
 
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
+def clean_text(text): 
+  tokens = nltk.word_tokenize(text)
+  tokens = [lemmatizer.lemmatize(word) for word in tokens]
+  return tokens
 
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
+def bag_of_words(text, vocab): 
+  tokens = clean_text(text)
+  bow = [0] * len(vocab)
+  for w in tokens: 
+    for idx, word in enumerate(vocab):
+      if word == w: 
+        bow[idx] = 1
+  return np.array(bow)
 
-    labels = sorted(labels)
+def pred_class(text, vocab, labels): 
+  bow = bag_of_words(text, vocab)
+  result = model.predict(np.array([bow]))[0]
+  thresh = 0.2
+  y_pred = [[idx, res] for idx, res in enumerate(result) if res > thresh]
 
-    training = []
-    output = []
+  y_pred.sort(key=lambda x: x[1], reverse=True)
+  return_list = []
+  for r in y_pred:
+    return_list.append(labels[r[0]])
+  return return_list
 
-    out_empty = [0 for _ in range(len(labels))]
+def get_response(intents_list, intents_json): 
+  tag = intents_list[0]
+  list_of_intents = intents_json["intents"]
+  for i in list_of_intents: 
+    if i["tag"] == tag:
+      result = random.choice(i["responses"])
+      break
+  return result
 
-    for x, doc in enumerate(docs_x):
-        bag = []
-
-        wrds = [stemmer.stem(w.lower()) for w in doc]
-
-        for w in words:
-            if w in wrds:
-                bag.append(1)
-            else:
-                bag.append(0)
-
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
-
-        training.append(bag)
-        output.append(output_row)
-
-
-    training = numpy.array(training)
-    output = numpy.array(output)
-
-    with open("data.pickle", "wb") as f:
-        pickle.dump((words, labels, training, output), f)
-
-ops.reset_default_graph()
-
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net = tflearn.regression(net)
-
-model = tflearn.DNN(net)
-
-try:
-    model.load("model.tflearn")
-except:
-    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-    model.save("model.tflearn")
-
-
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
-        
-    return numpy.array(bag)
-
-
-def chat():
-    print("Start talking with the bot (type quit to stop)!")
-    while True:
-        inp = input("You: ")
-        if inp.lower() == "quit":
-            break
-
-        results = model.predict([bag_of_words(inp, words)])
-        results_index = numpy.argmax(results)
-        tag = labels[results_index]
-
-        for tg in data["intents"]:
-            if tg['tag'] == tag:
-                responses = tg['responses']
-
-        print(random.choice(responses))
-
-chat()
+# running the chatbot
+while True:
+    message = input("You: ")
+    intents = pred_class(message, words, classes)
+    result = get_response(intents, data)
+    print("Average Warriors Fan: " + result)
+    if message in data["intents"][4]["responses"]:
+        break
