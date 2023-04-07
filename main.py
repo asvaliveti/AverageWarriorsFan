613d0e6551562c605b44073cba1654ce553be188
Anirudh Valiveti <asvaliveti@gmail.com>
2023-04-07 16:46:25 -0500

    stopped using main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from chatbot import Chatbot
-if __name__ == "__main__":
-    chatbot = Chatbot()
-    while True:
-        message = input("You: ")
-        response = chatbot.inputMessage(message)
-        print("Average Warriors Fan: " + response)
+# from chatbot import Chatbot
+# if __name__ == "__main__":
+#     chatbot = Chatbot()
+#     while True:
+#         message = input("You: ")
+#         response = chatbot.inputMessage(message)
+#         print("Average Warriors Fan: " + response)
