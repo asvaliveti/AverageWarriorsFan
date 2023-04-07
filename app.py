491e1de44a199d4816a9e826119bde292bd0d0b9
Anirudh Valiveti <asvaliveti@gmail.com>
2023-04-07 17:59:42 -0500

    changed post to get

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 chatbot = Chatbot()
 
 # Define a POST endpoint for receiving messages from the frontend chatbot
-@app.route('/message', methods=['POST'])
+@app.route('/message', methods=['GET'])
 def handle_message():
     message = request.json['message']
     # Process the message and generate a response
