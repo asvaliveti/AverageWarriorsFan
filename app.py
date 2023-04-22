5700879ffd3abb766573258ec041b0cb5a2b2343
Anirudh Valiveti <asvaliveti@gmail.com>
2023-04-21 22:46:57 -0500

    added header

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask, request, jsonify, make_response
 from chatbot import Chatbot
-from flask_cors import CORS, cross_origin
 import json
 
 app = Flask(__name__)
 chatbot = Chatbot()
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}, "methods": ["POST"]})
 
 # Define a POST endpoint for receiving messages from the frontend chatbot
 @app.route('/message', methods=['POST'])
-@cross_origin()
 def handle_message():
     message = request.json['message']
     # Process the message and generate a response
     output = chatbot.inputMessage(message)
     response = make_response(jsonify({'response': output}))
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
+    response.headers["Access-Control-Allow-Origin"] = '*'
     return response
 
 if __name__ == '__main__':
